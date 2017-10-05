9db8f305155837327c087b43e8490a987fe99703
Alexander Temerev <sorhed@gmail.com>
2017-10-06 00:53:19 +0200

    Scales

---
+++ web/js/app.js
@@ -0,0 +1,37 @@
+function main() {
+    const width = 500;
+    const height = 200;
+
+    const topScale = createVolumeScale(1, 100000, 0, 100);
+    const bottomScale = createVolumeScale(100000, 1, 0, 100);
+
+    const svg = prepareSvg("btc-usd-bitmex", width, height + 50);
+
+    const bidVolumeAxis = mkAxis(topScale).ticks(4).tickFormat(d3.format(",d"));
+    const offerVolumeAxis = mkAxis(bottomScale).ticks(4).tickFormat(d3.format(",d"));
+
+
+    svg.append("g")
+        .attr("class", "y1 axis").attr("transform", "translate(" + (width - 50) + ", " + 10 + ")")
+        .call(bidVolumeAxis);
+
+    svg.append("g")
+        .attr("class", "y2 axis").attr("transform", "translate(" + (width - 50) + ", " + (height / 2 + 10) + ")")
+        .call(offerVolumeAxis);
+}
+
+function prepareSvg(id, width, height) {
+    return d3.select("#" + id)
+        .attr("width", width)
+        .attr("height", height);
+}
+
+function mkAxis(scale) {
+    return d3.axisRight(scale);
+}
+
+function createVolumeScale(minVolume, maxVolume, minScale, maxScale) {
+    return d3.scaleLog().domain([minVolume, maxVolume]).range([maxScale, minScale]);
+}
+
+main();
