18702cad852d1d1f4bdfbb8294abf572bd5c06f2
Alexander Temerev <sorhed@gmail.com>
2018-06-26 00:28:22 +0200

    Getting trades feed from Bitmex

--- web/js/app.js
+++ web/js/app.js
@@ -1,6 +1,16 @@
+let conf = {
+    timeSpanS: 60,
+    timeResMs: 100,
+    timeAxisHeight: 50,
+    symbol: "XBTUSD",
+    bucketMs: 5
+}
+
+let trades = []
+
 let main = () => {
     let now = new Date()
-    let span = 60 // seconds
+    let span = conf.timeSpanS // seconds
     let svg = prepareSvg("#chart", "chart-flow")
     let scale = mkTimeScale(svg, now, span)
     let axis = mkTimeAxis(scale)
@@ -8,7 +18,41 @@ let main = () => {
     window.setInterval(() => {
         let update = () => updateTimeAxis(span)
         requestAnimationFrame(update)
-    }, 100)
+    }, conf.timeResMs)
+    connectWs((data) => onWsMessage(JSON.parse(data)))
+}
+
+let onWsMessage = (data) => {
+    if (data.table === "trade") {
+        updateTrades(data)
+    }
+}
+
+let isNewBucket = (data, item, bucketMs) => {
+    if (data.length === 0) {
+        return true
+    } else {
+        let last = data[data.length - 1]
+        let sameSide = Math.sign(last[1]) === Math.sign(item[1])
+        let sameBucket = item[0] - last[0] < bucketMs
+        return !(sameSide && sameBucket)
+    }
+}
+
+let connectWs = (callback) => {
+    let ws = new WebSocket("wss://www.bitmex.com/realtime?subscribe=trade")
+    ws.onopen = (conn) => {
+        console.log("WS connected")
+    }
+    ws.onmessage = (e) => {
+        callback(e.data)
+    }
+    ws.onclose = (e) => {
+        console.log("WS disconnected")
+    }
+    ws.onerror = (e) => {
+        console.log(e)
+    }
 }
 
 let prepareSvg = (containerSelector, name) => {
@@ -17,20 +61,49 @@ let prepareSvg = (containerSelector, name) => {
         .attr("id", name)
 }
 
+let updateTrades = (data) => {
+    for (let item of data.data) {
+        if (item.symbol === conf.symbol) {
+            let timeSpan = conf.timeSpanS * 1000
+            let side = item.side
+            let amount = side === "Buy" ? item.size : -item.size
+            let price = item.price
+            let ts = new Date(item.timestamp).getTime()
+            let newItem = [ts, amount, price]
+            if (isNewBucket(trades, newItem, conf.bucketMs)) {
+                trades.push([ts, amount, price])
+            } else {
+                let last = trades[trades.length - 1]
+                let lastPrice = last[2]
+                let lastAmount = last[1]
+                let newAmount = newItem[1]
+                let newPrice = newItem[2]
+                let vwap = (lastPrice * lastAmount + newPrice * newAmount) / (lastAmount + newAmount)
+                let amount = lastAmount + newAmount
+                last[1] = amount
+                last[2] = vwap
+            }
+            while (trades[trades.length - 1][0] - trades[0][0] > timeSpan) {
+                trades.shift()
+            }
+        }
+    }
+}
+
 let updateTimeAxis = (span) => {
     let svg = d3.select("svg")
     let now = new Date()
     let scale = mkTimeScale(svg, now, span)
     let axis = mkTimeAxis(scale)
     let t = d3.transition()
-        .duration(100)
+        .duration(conf.timeResMs)
         .ease(d3.easeLinear)
     svg.select(".time").transition(t).call(axis)
 }
 
 let renderTimeAxis = (svg, axis) => {
     let bbox = svg.node().getBoundingClientRect()
-    let y = bbox.height / 2
+    let y = bbox.height - conf.timeAxisHeight
     svg.append("g")
         .attr("class", "time axis")
         .attr("transform", "translate(" + 0 + ", " + y + ")")
